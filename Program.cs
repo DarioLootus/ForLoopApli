8a99fe5a75c23affc8257410e4c7043d5ce8047e
opilane <150146709+DarioLootus@users.noreply.github.com>
2024-05-03 12:58:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace ForLoopApli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ruut");
+            Console.WriteLine("Sisesta ruudu suurus");
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
